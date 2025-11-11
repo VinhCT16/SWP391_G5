873bdcecc7cec8c8a89b0b3e9a5c340600f8ee5a
VinhCT16 <vinhcthe170468@fpt.edu.vn>
2025-11-12 06:57:18 +0700

    Update requestRoutes.js

--- server/routes/requestRoutes.js
+++ server/routes/requestRoutes.js
@@ -4,7 +4,10 @@ const {
   getAllRequests,
   getMyRequests,
   updateRequestStatus,
-  createRequest
+  createRequest,
+  getRequestById,
+  getAvailableStaffForRequest,
+  assignStaffToRequest
 } = require("../controllers/requestController");
 const auth = require("../utils/authMiddleware");
 const { requireManager, requireCustomer } = require("../utils/authMiddleware");
@@ -14,10 +17,15 @@ const router = express.Router();
 // Manager routes
 router.get("/all", auth, requireManager, getAllRequests);
 router.put("/:requestId/status", auth, requireManager, updateRequestStatus);
+router.get("/:id/available-staff", auth, requireManager, getAvailableStaffForRequest);
+router.post("/:id/assign-staff", auth, requireManager, assignStaffToRequest);
 
 // Customer routes
 router.get("/my", auth, requireCustomer, getMyRequests);
 router.post("/", auth, requireCustomer, createRequest);
+
+// General routes (must be last to avoid conflicts)
+router.get("/:id", auth, getRequestById);
 
 module.exports = router;
 
